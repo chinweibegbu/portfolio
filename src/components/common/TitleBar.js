35a6f8978042a8909d755570070308e14924f995
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-11-07 22:43:47 +0100

    Updated TitleBar and Footer

--- src/components/common/TitleBar.js
+++ src/components/common/TitleBar.js
@@ -2,10 +2,9 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import '../../styles/TitleBar.css';
 
-
 function TitleBar() {
     return (
-        <nav className="TitleBar navbar navbar-expand-lg navbar-dark bg-dark py-3">
+        <nav className="TitleBar navbar navbar-expand-lg navbar-dark py-3">
             <div className="container">
                 <p className="navbar-brand my-auto" href="index.html"><span className="fw-bolder">Portfolio</span></p>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
