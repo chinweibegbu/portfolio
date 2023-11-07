f32a0339bebd79eaf47f985535a2bc8f0545520a
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-11-07 22:48:18 +0100

    Added Landing return to TitleBar

--- src/components/common/TitleBar.js
+++ src/components/common/TitleBar.js
@@ -6,7 +6,9 @@ function TitleBar() {
     return (
         <nav className="TitleBar navbar navbar-expand-lg navbar-dark py-3">
             <div className="container">
-                <p className="navbar-brand my-auto" href="index.html"><span className="fw-bolder">Portfolio</span></p>
+                <NavLink to="/">
+                    <p className="navbar-brand my-auto" href="index.html"><span className="fw-bolder">My Portfolio</span></p>
+                </NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto small">
