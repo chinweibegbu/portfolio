f438900dca930a761c5e1497d339c812c645bfdb
chinweibegbu <chinwe.ibegbu@gmail.com>
2023-11-07 15:52:26 +0100

    Updated style

--- src/components/common/TitleBar.js
+++ src/components/common/TitleBar.js
@@ -1,22 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import '../../styles/TitleBar.css';
+
 
 function TitleBar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-white bg-white py-3">
-            <div className="container px-5">
-                <a className="navbar-brand" href="index.html"><span className="fw-bolder">Chinwe Ibegbu</span></a>
+        <nav className="TitleBar navbar navbar-expand-lg navbar-dark bg-dark py-3">
+            <div className="container">
+                <p className="navbar-brand my-auto" href="index.html"><span className="fw-bolder">Portfolio</span></p>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
+                    <ul className="navbar-nav ms-auto small">
                         <NavLink to="/designer">
-                            <li className="nav-item"><a className="nav-link">Designer</a></li>
+                            <li className="nav-item"><p className="nav-link">Designer</p></li>
                         </NavLink>
                         <NavLink to="/developer">
-                            <li className="nav-item"><a className="nav-link">Developer</a></li>
+                            <li className="nav-item"><p className="nav-link">Developer</p></li>
                         </NavLink>
                         <NavLink to="/volunteer">
-                            <li className="nav-item"><a className="nav-link">Volunteer</a></li>
+                            <li className="nav-item"><p className="nav-link">Volunteer</p></li>
                         </NavLink>
                     </ul>
                 </div>
