329dc04b2e94c76f28c5b6f6c966ff4d32c7f443
chinweibegbu <chinwe.ibegbu@gmail.com>
2024-05-05 19:05:53 +0100

    Replace logo

---
+++ src/components/common/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../../styles/Logo.css';
+
+function Logo() {
+    return (
+        <div className="Logo py-1 px-2">
+            chinwe.
+        </div>
+    );
+}
+
+export default Logo;
