966209e9edce4e1df345ccf22b9e35a0304f5f2a
chinweibegbu <chinwe.ibegbu@gmail.com>
2024-05-07 19:17:46 +0100

    Fix incorrect DOM component nesting (<div> inside <p>)

--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -3,7 +3,7 @@ import '../../styles/Logo.css';
 
 function Logo() {
     return (
-        <div className="Logo py-1 px-2">
+        <div className="Logo navbar-brand my-auto py-1 px-2" href="index.html">
             chinwe.
         </div>
     );
--- src/components/common/TitleBar.js
+++ src/components/common/TitleBar.js
@@ -8,7 +8,7 @@ function TitleBar() {
         <nav className="TitleBar navbar navbar-expand-lg py-2">
             <div className="container">
                 <NavLink to="/">
-                    <p className="navbar-brand my-auto" href="index.html"><Logo /></p>
+                    <Logo />
                 </NavLink>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
