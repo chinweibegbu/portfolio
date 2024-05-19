62d2e4e9db2dc69c30fdaf7db371a5df4b49de11
chinweibegbu <chinwe.ibegbu@gmail.com>
2024-05-20 00:31:21 +0100

    Re-implement and re-insert UnderConstruction component

--- src/components/pages/Volunteer.js
+++ src/components/pages/Volunteer.js
@@ -1,10 +1,13 @@
 import React from "react";
+
 import PageHeader from "../common/PageHeader.js";
+import UnderContruction from "../common/UnderConstruction.js";
 
 function Volunteer() {
     return (
         <div className="Volunteer container Container">
             <PageHeader title="Volunteer" type="page" />
+            <UnderContruction />
         </div>
     );
 }
