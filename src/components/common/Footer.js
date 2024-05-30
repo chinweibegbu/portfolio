a5c7e245496bc28104a72480cac8cad1394b6a3f
chinweibegbu <chinwe.ibegbu@gmail.com>
2024-05-31 00:24:47 +0100

    Update footer text

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
             <div className="container h-100">
                 <div className="row h-100 flex-column flex-sm-row align-content-center">
                     <div className="col-auto">
-                        <div className="small m-0">Copyright &copy; Chinwe Ibegbu - Portfolio 2024</div>
+                        <div className="small m-0">&copy; Chinwe Ibegbu - Portfolio 2024</div>
                     </div>
                 </div>
             </div>
